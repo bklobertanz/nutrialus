fd98842efbc4dd329947349e0b92d8824b2599da
Barís Klobertanz <klobertanz@gmail.com>
2021-03-27 20:13:23 -0300

    Estado inicial.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import {Button, Paper} from '@material-ui/core';
+import axios from 'axios';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [usuario, setUsuario] = useState({});
+  const URL = "https://0q27loouph.execute-api.us-east-1.amazonaws.com/"
+  const handleOnClick= async() =>{
+    const res = await axios.get(URL);
+    if(res.status === 200){
+      setUsuario({
+        nombre: res.data.name,
+        email: res.data.email,
+        phone: res.data.phone,
+        image: res.data.image,
+      })
+    }
+    console.log(usuario);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper elevation={3} />
+      <Button onClick={handleOnClick} variant="contained" color="primary">
+          cambiar
+      </Button>
+      <div>
+        <h1>Usuario</h1>
+        <hr/>
+      </div>
     </div>
   );
 }
